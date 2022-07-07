4f0010c0c848c20de758c03e011d906778e2276d
zzerowin <alaykr123@gmail.com>
2022-07-07 14:31:35 +0900

    명령 프롬프트 구현

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,8 +1,24 @@
 package com.ll.exam;
 
+import java.util.Scanner;
+
 public class App {
 
     public void run() {
         System.out.println("-- 명연 SSG --");
+
+        Scanner sc = new Scanner(System.in);
+
+        outer:
+        while (true) {
+            String cmd = sc.nextLine().trim();
+
+            switch (cmd) {
+                case "종료":
+                    break outer;
+            }
+        }
+
+        sc.close();
     }
 }
