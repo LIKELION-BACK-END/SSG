4da36ca87b2012bacd98a5ddceceb78f130542d5
zzerowin <alaykr123@gmail.com>
2022-07-08 13:50:00 +0900

    App테스트 추가

---
+++ src/test/java/com/ll/exam/AppTest.java
@@ -0,0 +1,14 @@
+package com.ll.exam;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    void 테스트_실험() {
+        int res = 10 + 20;
+        assertEquals(30, res);
+    }
+}
