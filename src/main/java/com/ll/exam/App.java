1f754e87cd42ebd3a49d670e182fa18674fa26b2
zzerowin <alaykr123@gmail.com>
2022-07-12 11:33:19 +0900

    [수정] Rq에 Map 도입

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -16,7 +16,7 @@ public class App {
     }
 
     public void run() {
-        System.out.println("== 명언 SSG ==");
+        System.out.println("-- 명언 SSG --");
 
         outer:
         while (true) {
